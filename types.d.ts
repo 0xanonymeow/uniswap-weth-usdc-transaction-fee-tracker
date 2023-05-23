05e635addd2996e53898eba099032d60b867cc2d
0xanonymeow <anonymeow@incatni.to>
2023-05-23 11:00:07 +0700

    feat: add transaction api

---
+++ types.d.ts
@@ -0,0 +1,27 @@
+type APIResponse<T> = {
+  status: string;
+  message: string;
+  result: Array<T>;
+};
+
+type Transaction = {
+  blockNumber: string;
+  timeStamp: string;
+  hash: string;
+  nonce: string;
+  blockHash: string;
+  from: string;
+  contractAddress: string;
+  to: String;
+  value: string;
+  tokenName: string;
+  tokenSymbol: string;
+  tokenDecimal: string;
+  transactionIndex: string;
+  gas: string;
+  gasPrice: string;
+  gasUsed: string;
+  cumulativeGasUsed: string;
+  input: string;
+  confirmations: string;
+};
