3ea845b9b7aa586d4fed29226ddffc4dae1d90d9
0xanonymeow <anonymeow@incatni.to>
2023-05-24 16:38:52 +0700

    refactor: transaction endpoint

--- app/api/transaction/route.ts
+++ app/api/transaction/route.ts
@@ -1,3 +1,44 @@
-import { NextResponse } from 'next/server';
+import prisma from '@/lib/prisma';
+import { PrismaClient } from '@prisma/client';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
+import { NextRequest, NextResponse } from 'next/server';
 
-export const GET = async () => NextResponse.json({});
+export const GET = async (request: NextRequest) => {
+  const { searchParams } = request.nextUrl;
+  const id = searchParams.get('id');
+  const page = searchParams.get('page') || '1';
+  const offset = searchParams.get('offset') || '50';
+
+  let transactions = [];
+
+  try {
+    if (id) {
+      transactions = await (
+        prisma as PrismaClient
+      ).transaction.findFirstOrThrow({
+        where: {
+          hash: id,
+          tokenSymbol: 'USDC',
+        },
+      });
+    } else {
+      transactions = await (
+        prisma as PrismaClient
+      ).transaction.findMany({
+        orderBy: {
+          date: 'desc',
+        },
+        skip: (Number(page) - 1) * Number(offset),
+        take: Number(offset),
+      });
+    }
+
+    return NextResponse.json({ data: transactions });
+  } catch (e) {
+    if (e instanceof PrismaClientKnownRequestError) {
+      if (e.code === 'P2025')
+        return new Response('Not Found', { status: 404 });
+    }
+    return new Response('Internal Server Error', { status: 500 });
+  }
+};
