1ef96ee56a185cdb5f2dda6a055813452a580f00
0xanonymeow <anonymeow@incatni.to>
2023-05-25 17:28:40 +0700

    feat: add searchbar, table to query and display data from weth-usdc pool

--- app/page.tsx
+++ app/page.tsx
@@ -1,114 +1,110 @@
-import Image from 'next/image';
+'use client';
+
+import { useGetEthUsdcPrice } from '@/hooks/query/useGetEthUsdcPrice';
+import { useLazyGetTranasaction } from '@/hooks/query/useGetTransaction';
+import { useGetUsdcUsdPrice } from '@/hooks/query/useGetUsdcUsdPrice';
+import { getFeeInUSD, getValueInUSD } from '@/lib/utils';
+import { Magnifier } from '@/public/icons/magnifier';
+import { map } from 'lodash';
+import { useMemo, useState } from 'react';
 
 function App() {
+  const [params, setParams] = useState<GetTransaction>({});
+
+  const {
+    data,
+    // isLoading: isGetTransactionLoading,
+    refetch,
+  } = useLazyGetTranasaction(params);
+  const {
+    data: price,
+    // isLoading: isGetEthUsdcPriceLoading
+  } = useGetEthUsdcPrice();
+  const {
+    data: usdcUsd,
+    // isLoading: isGetUsdcUsdPriceLoading
+  } = useGetUsdcUsdPrice();
+
+  const priceInUsd = useMemo(
+    () => ({ WETH: price, USDC: usdcUsd }),
+    [price, usdcUsd],
+  );
+
+  const onSearch = () => {
+    refetch();
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <main className="min-h-screen min-w-screen">
+      <div className="p-32 flex flex-col justify-center">
+        <div className="w-full flx gap-2 flex align-middle justify-center">
+          <input
+            className="w-1/2 h-8 rounded-md px-4 "
+            value={params.id}
+            onChange={(e) =>
+              setParams({ ...params, id: e.target.value })
+            }
+            placeholder="0x3d4f354..."
+          />
+          <button
+            type="button"
+            onClick={onSearch}
+            className="w-8 h-8"
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+            <Magnifier />
+          </button>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course
-            with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with
-            Vercel.
-          </p>
-        </a>
+        <div className="w-full mt-16 flex justify-center">
+          <table className="w-2/3 text-white dark:text-slate-800">
+            <thead
+              className="text-center bg-slate-600 dark:bg-slate-400"
+              style={{
+                borderRadius: 4,
+              }}
+            >
+              <tr className="text-white">
+                <th>id</th>
+                <th>fee</th>
+                <th>date</th>
+                <th>executed price</th>
+                <th>token</th>
+              </tr>
+            </thead>
+            <tbody className="text-end dark:bg-slate-100 bg-slate-500">
+              {map(
+                data,
+                (
+                  {
+                    hash,
+                    fee,
+                    date,
+                    value,
+                    tokenName,
+                    tokenDecimal,
+                    tokenSymbol,
+                  },
+                  i,
+                ) => (
+                  <tr key={i} className="">
+                    <td>{hash}</td>
+                    <td>${getFeeInUSD(fee)}</td>
+                    <td>{new Date(date).toLocaleString()}</td>
+                    <td>
+                      $
+                      {getValueInUSD(
+                        value,
+                        tokenDecimal,
+                        tokenSymbol,
+                        priceInUsd,
+                      )}
+                    </td>
+                    <td className="p-2">{tokenName}</td>
+                  </tr>
+                ),
+              )}
+            </tbody>
+          </table>
+        </div>
       </div>
     </main>
   );
