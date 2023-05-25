eb02c8f20d73ca2aaf24f870b4672e92b1a6aaa6
0xanonymeow <anonymeow@incatni.to>
2023-05-25 17:28:40 +0700

    feat: add searchbar, table to query and display data from weth-usdc pool

---
+++ public/icons/magnifier.tsx
@@ -0,0 +1,29 @@
+export const Magnifier = ({ className }: { className?: string }) => {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '100%',
+        height: '100%',
+      }}
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="currentColor"
+        className={className ? className : 'w-6 h-6 '}
+        preserveAspectRatio="xMaxYMax meet"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+        />
+      </svg>
+    </div>
+  );
+};
