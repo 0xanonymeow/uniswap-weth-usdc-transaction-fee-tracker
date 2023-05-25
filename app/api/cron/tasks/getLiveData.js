c69dd6e12560215828f6297c7d40648feb98d993
0xanonymeow <anonymeow@incatni.to>
2023-05-25 17:24:02 +0700

    fix: fee unit base, error handling

--- app/api/cron/tasks/getLiveData.js
+++ app/api/cron/tasks/getLiveData.js
@@ -1,5 +1,6 @@
 // @ts-nocheck
 
+const { isString } = require('lodash');
 const {
   UNISWAP_WETH_USDC_PAIR_CONTRACT_ADDRESS,
 } = require('../../../../constants/contracts');
@@ -8,10 +9,11 @@ const { map } = require('lodash');
 
 const getLiveData = async (
   page = '1',
-  offset = '100',
+  offset = '1000',
   sort = 'desc',
 ) => {
   const baseUrl = process.env.ETHERSCAN_BASE_URL;
+
   const params = new URLSearchParams({
     module: 'account',
     action: 'tokentx',
@@ -20,42 +22,52 @@ const getLiveData = async (
     offset,
     sort,
   });
+
   const apiKey = process.env.ETHERSCAN_API_KEY;
   if (apiKey) params.append('apikey', apiKey); // if apiKey is undefined, rate limit of 1 request per 5 seconds will be applied
-  const res = await fetch(`${baseUrl}?${params}`);
-  const data = await res.json();
-  const { result } = data;
 
-  const transformedData = map(
-    result,
-    ({
-      hash,
-      timeStamp,
-      from,
-      value,
-      tokenName,
-      tokenSymbol,
-      tokenDecimal,
-      gasUsed,
-      gasPrice,
-      confirmations,
-    }) => ({
-      hash,
-      date: new Date(Number(timeStamp) * 1000),
-      from,
-      value,
-      tokenName,
-      tokenSymbol,
-      tokenDecimal,
-      fee: String(BigInt(gasUsed) * BigInt(gasPrice)),
-      confirmations,
-    }),
-  ).filter(({ confirmations }) => Number(confirmations) > 0);
-  const { count } = await prisma.transaction.createMany({
-    data: transformedData,
-    skipDuplicates: true,
-  });
-  return count;
+  try {
+    const res = await fetch(`${baseUrl}?${params}`);
+    const data = await res.json();
+    const { result } = data;
+
+    if (isString(result)) throw new Error(result);
+
+    const transformedData = map(
+      result,
+      ({
+        hash,
+        timeStamp,
+        from,
+        value,
+        tokenName,
+        tokenSymbol,
+        tokenDecimal,
+        gasUsed,
+        gasPrice,
+        confirmations,
+      }) => ({
+        hash,
+        date: new Date(Number(timeStamp) * 1000),
+        from,
+        value,
+        tokenName,
+        tokenSymbol,
+        tokenDecimal,
+        fee: String((Number(gasUsed) * Number(gasPrice)) / 10 ** 9), // gasPrice is denoted in gwei, convert it to eth for later calculation
+        confirmations,
+      }),
+    ).filter(({ confirmations }) => Number(confirmations) > 0);
+
+    const { count } = await prisma.transaction.createMany({
+      data: transformedData,
+      skipDuplicates: true,
+    });
+    console.log(`inserted ${count} records`);
+    return count;
+  } catch (e) {
+    console.error(e);
+  }
 };
 
 module.exports = {
