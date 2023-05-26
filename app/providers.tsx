46c80853b160176a41d906040f86378c7ab3a742
0xanonymeow <anonymeow@incatni.to>
2023-05-26 19:23:57 +0700

    feat: add wagmi

--- app/providers.tsx
+++ app/providers.tsx
@@ -2,13 +2,31 @@
 
 import { ReactNode } from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
+import {
+  WagmiConfig,
+  configureChains,
+  createConfig,
+  mainnet,
+} from 'wagmi';
+import { publicProvider } from 'wagmi/providers/public';
+
+const { chains, publicClient, webSocketPublicClient } =
+  configureChains([mainnet], [publicProvider()]);
+
+const config = createConfig({
+  autoConnect: true,
+  publicClient,
+  webSocketPublicClient,
+});
 
 const queryClient = new QueryClient();
 
 export function Providers({ children }: { children: ReactNode }) {
   return (
-    <QueryClientProvider client={queryClient}>
-      {children}
-    </QueryClientProvider>
+    <WagmiConfig config={config}>
+      <QueryClientProvider client={queryClient}>
+        {children}
+      </QueryClientProvider>
+    </WagmiConfig>
   );
 }
