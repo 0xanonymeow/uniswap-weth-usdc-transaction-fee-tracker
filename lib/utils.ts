54c95618f553bafa55e4338a9af31e236c741fc6
0xanonymeow <anonymeow@incatni.to>
2023-05-27 22:14:33 +0700

    feat: add pagination in response

--- lib/utils.ts
+++ lib/utils.ts
@@ -53,3 +53,24 @@ export const getValueInUsd = (
 
   return usdAmount.toFixed(2);
 };
+
+export const paginatedResponse = (
+  data: [unknown[], number],
+  page: number = 1,
+  offset: number = 50,
+) => {
+  const [result, total] = data;
+  const lastPage = Math.ceil(total / offset);
+  const nextPage = page + 1 > lastPage ? null : page + 1;
+  const prevPage = page - 1 < 1 ? null : page - 1;
+
+  return {
+    statusCode: 'success',
+    data: [...result],
+    count: total,
+    currentPage: page,
+    nextPage,
+    prevPage,
+    lastPage,
+  };
+};
