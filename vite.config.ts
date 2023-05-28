60b28991a0e5563ae1b0517c8ec10f818d1ce141
0xanonymeow <anonymeow@incatni.to>
2023-05-28 21:22:37 +0700

    test: update config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,22 @@
-import { configDefaults, defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
+import { configDefaults, defineConfig } from 'vitest/config';
 
 export default defineConfig({
   plugins: [react()],
   test: {
-    exclude: [...configDefaults.exclude, '*.js'],
+    exclude: [...configDefaults.exclude],
+    coverage: {
+      enabled: true,
+      all: true,
+      exclude: [
+        '*.d.ts',
+        '*.config.ts',
+        '.next/**',
+        '**/prisma.ts',
+        'middleware.ts',
+      ],
+      excludeNodeModules: true,
+      extension: ['.ts'],
+    },
   },
 });
