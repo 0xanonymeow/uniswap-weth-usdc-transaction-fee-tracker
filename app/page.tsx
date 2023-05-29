d0d31cedd7adca4a5d009fa13de801cf7f6847cf
0xanonymeow <anonymeow@incatni.to>
2023-05-29 09:29:58 +0700

    feat: add pagination, datetime picker

--- app/page.tsx
+++ app/page.tsx
@@ -3,59 +3,22 @@
 import { useGetEthUsdcPrice } from '@/hooks/query/useGetEthUsdcPrice';
 import { useLazyGetTranasaction } from '@/hooks/query/useGetTransaction';
 import { useGetUsdcUsdPrice } from '@/hooks/query/useGetUsdcUsdPrice';
-import { getFeeInUsd, getValueInUsd } from '@/lib/utils';
+import { getFeeInUsd, getValueInUsd } from '@/lib/clientUtils';
 import { Magnifier } from '@/public/icons/magnifier';
-import { isEmpty, isUndefined, map } from 'lodash';
+import { map } from 'lodash';
+import moment, { Moment } from 'moment';
 import { useEffect, useMemo, useState } from 'react';
-import { useTransaction } from 'wagmi';
+import toast from 'react-hot-toast';
+import DateTimePicker from 'react-tailwindcss-datetimepicker';
 
 function App() {
   const [params, setParams] = useState<GetTransaction>({
-    id: '',
     page: '1',
-    offset: '50',
-  });
-  const [isSearching, setIsSearching] = useState(false);
-
-  const {
-    data: getTransactionData,
-    // isLoading: isGetTransactionLoading,
-    isError: isGetTransactionError,
-    refetch: refetchGetTransaction,
-  } = useLazyGetTranasaction({
-    ...params,
-    options: {
-      onSettled: () => setIsSearching(false),
-      refetchOnWindowFocus: false,
-    },
+    take: '50',
   });
 
   const {
-    data: transaction,
-    refetch: refetchTransaction,
-    isError: isTransactionError,
-    isSuccess: isTransactionSuccess,
-  } = useTransaction({
-    hash: params?.id,
-    enabled: false,
-    onSettled: () => setIsSearching(false),
-    refetchOnWindowFocus: false,
-  });
-
-  useEffect(() => {
-    if (!isEmpty(getTransactionData)) return;
-    if (!isUndefined(transaction)) {
-      const { blockNumber } = transaction;
-      setParams({
-        ...params,
-        startblock: Number(blockNumber),
-        endblock: Number(blockNumber),
-      });
-    }
-  }, [isSearching, getTransactionData, transaction, params]);
-
-  const {
-    data: price,
+    data: ethPrice,
     // isLoading: isGetEthUsdcPriceLoading
   } = useGetEthUsdcPrice();
   const {
@@ -64,60 +27,149 @@ function App() {
   } = useGetUsdcUsdPrice();
 
   const priceInUsd = useMemo(
-    () => ({ WETH: price, USDC: usdcUsd }),
-    [price, usdcUsd],
+    () => ({ WETH: ethPrice, USDC: usdcUsd }),
+    [ethPrice, usdcUsd],
   );
 
+  const {
+    data,
+    refetch,
+    error: getTransactionError,
+  } = useLazyGetTranasaction(params);
+
+  console.log(params);
+  const lastPage = useMemo(() => data?.lastPage || 1, [data]);
+
+  useEffect(() => {
+    if (getTransactionError) toast.error(getTransactionError);
+  }, [getTransactionError]);
+
+  const now = new Date();
+  const start = moment(
+    new Date(
+      Date.UTC(
+        now.getFullYear(),
+        now.getMonth(),
+        now.getDate(),
+        0,
+        0,
+        0,
+        0,
+      ),
+    ),
+  ).utc();
+  const end = moment(start).add(1, 'days').subtract(1, 'seconds');
+  const [range, setRange] = useState({
+    start,
+    end,
+  });
+  const ranges = {
+    Today: [moment(start), moment(end)],
+  };
+  const local = {
+    format: 'DD-MM-YYYY HH:mm',
+    sundayFirst: false,
+  };
+  function handleApply(startDate: Moment, endDate: Moment) {
+    setRange({ start: startDate, end: endDate });
+  }
+
   const onSearch = () => {
-    setIsSearching(true);
-    refetchGetTransaction({
-      throwOnError: false,
-      cancelRefetch: true,
-    });
+    const startDate = new Date(range.start.format()).toISOString();
+    const endDate = new Date(range.end.format()).toISOString();
+    refetch();
   };
 
-  const onTxNotFound = () => {
-    if (!isSearching) return;
-    refetchTransaction({ throwOnError: false, cancelRefetch: true });
+  const onSetPage = (e: { target: { value: string } }) => {
+    setParams({ ...params, page: e.target.value });
+  };
+
+  const onSetItemsPerPage = (e: { target: { value: string } }) => {
+    setParams({ ...params, take: e.target.value });
   };
 
   useEffect(() => {
-    if (!isSearching || !params?.id) return;
-    if (isEmpty(getTransactionData)) onTxNotFound();
-  }, [getTransactionData, params, isSearching]);
-
-  useEffect(() => {
-    if (isTransactionSuccess && params?.startblock) {
-      refetchGetTransaction();
-    }
-    if (isTransactionError) {
-    }
-  }, [isTransactionError, isTransactionSuccess, params]);
+    refetch()
+  }, [params, refetch])
 
   return (
-    <main className="min-h-screen min-w-screen">
-      <div className="p-32 flex flex-col justify-center">
-        <div className="w-full flx gap-2 flex align-middle justify-center">
-          <input
-            className="w-1/2 h-8 rounded-md px-4 dark:text-black"
-            value={params.id}
-            onChange={(e) =>
-              setParams({ ...params, id: e.target.value })
-            }
-            placeholder="0x3d4f354..."
-          />
-          <button
-            type="button"
-            onClick={onSearch}
-            className="w-8 h-8"
-          >
-            <Magnifier />
-          </button>
+    <main className="min-w-screen min-h-screen">
+      <div className="flex flex-col justify-center p-32">
+        <div className="">
+          <div className="flx flex w-full justify-center gap-2 align-middle">
+            <input
+              className="h-8 w-1/2 rounded-md px-4 dark:text-black"
+              value={params.id}
+              onChange={(e) =>
+                setParams({ ...params, id: e.target.value })
+              }
+              placeholder="0x3d4f354..."
+            />
+            <button
+              type="button"
+              onClick={onSearch}
+              className="h-8 w-8"
+            >
+              <Magnifier />
+            </button>
+          </div>
+          <div className="flex justify-center mt-28">
+            {/* @ts-ignore */}
+            <DateTimePicker
+              ranges={ranges}
+              start={range.start}
+              end={range.end}
+              local={local}
+              applyCallback={handleApply}
+              smartMode
+            >
+              <input
+                className="w-[400px] rounded-md py-4  text-center bg-slate-400 text-white"
+                placeholder="Date range"
+                value={`${range.start.format(
+                  'DD-MM-YYYY HH:mm',
+                )} - ${range.end.format('DD-MM-YYYY HH:mm')}`}
+                // value={`${range.start} - ${range.end}`}
+                disabled
+              />
+            </DateTimePicker>
+          </div>
         </div>
-        <div className="w-full mt-16 flex flex-col align-middle justify-center">
+        <div className="mt-16 flex w-full flex-col justify-center align-middle">
+          <div className="flex w-full justify-end">
+            <div className="mb-4 flex w-auto gap-2">
+              <label htmlFor="page" className="self-center">
+                Page
+              </label>
+              <select
+                id="page"
+                className="min-w-[50px] text-center"
+                onChange={onSetPage}
+              >
+                {map([...Array(lastPage)], (_, i) => (
+                  <option key={i}>{i + 1}</option>
+                ))}
+              </select>
+              <label htmlFor="items-per-page" className="self-center">
+                Items per page
+              </label>
+              <select
+                id="items-per-page"
+                className="min-w-[50px] text-center"
+                onChange={onSetItemsPerPage}
+              >
+                <option>50</option>
+                <option>100</option>
+                <option>150</option>
+                <option>200</option>
+                <option>250</option>
+                <option>300</option>
+              </select>
+            </div>
+          </div>
           <table className="w-full text-white dark:text-slate-800">
             <thead
-              className="text-center bg-slate-600 dark:bg-slate-400"
+              className="bg-slate-600 text-center dark:bg-slate-400"
               style={{
                 borderRadius: 4,
               }}
@@ -130,9 +182,9 @@ function App() {
                 <th>token</th>
               </tr>
             </thead>
-            <tbody className="text-end dark:bg-slate-100 bg-slate-500">
+            <tbody className="bg-slate-500 text-end dark:bg-slate-100">
               {map(
-                getTransactionData,
+                data?.data,
                 (
                   {
                     hash,
@@ -142,12 +194,12 @@ function App() {
                     tokenName,
                     tokenDecimal,
                     tokenSymbol,
-                  },
-                  i,
+                  }: TransformedTransaction,
+                  i: number,
                 ) => (
                   <tr key={i}>
                     <td>{hash}</td>
-                    <td>${getFeeInUsd(fee, price)}</td>
+                    <td>${getFeeInUsd(fee, ethPrice)}</td>
                     <td>{new Date(date).toLocaleString()}</td>
                     <td>
                       $
@@ -164,7 +216,7 @@ function App() {
               )}
             </tbody>
           </table>
-          {isTransactionError && (
+          {getTransactionError && (
             <div className="w-full text-center mt-32">
               <p className="text-4xl">Not Found</p>
             </div>
